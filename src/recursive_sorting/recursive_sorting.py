a566ca1e17415abc4bf8cf3cc00da6dde1ae3a88
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-09-13 10:54:40 -0700

    Update recursive_sorting.py

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -11,4 +11,16 @@ def merge( arrA, arrB ):
 def merge_sort( arr ):
     # TO-DO
 
+    if len(arr) > 1:
+        # Split into left and right arrays (lists)
+        midway_point = len(arr) // 2 # This divides and returns an int, it will never return a float. 
+        left_array = arr[ : midway_point ]
+        right_array = arr[ midway_point : ]
+
+        left = merge_sort(left_array)
+        right = merge_sort(right_array)
+
+        arr = merge(left, right)
+
+
     return arr
